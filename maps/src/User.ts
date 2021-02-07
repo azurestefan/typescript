25f26b168eec25995fae69b01bbf12aa2d08646a
jeongminhong <azurestefan@gmail.com>
2021-02-07 23:23:16 +0900

    mapapp

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,17 +1,23 @@
-import faker from 'faker'; //nned to install the type declaration file. npm install @types/faker
+import faker from 'faker';
+import { Mappable } from './CustomMap';
 
-export class User {
-    name: string;
-    location: {
-        lat: number;
-        lng: number;
+export class User implements Mappable {
+  name: string;
+  location: {
+    lat: number;
+    lng: number;
+  };
+  color: string = 'red';
+
+  constructor() {
+    this.name = faker.name.firstName();
+    this.location = {
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude())
     };
+  }
 
-    constructor () {
-        this.name = faker.name.firstName();
-        this.location = {
-            lat: parseFloat(faker.address.latitude()),
-            lng: parseFloat(faker.address.longitude())
-        }
-    }
+  markerContent(): string {
+    return `User Name: ${this.name}`;
+  }
 }
