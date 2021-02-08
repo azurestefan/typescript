9efcc943aedcf678bed390110853dd03b8d25d6e
jeongminhong <azurestefan@gmail.com>
2021-02-08 23:11:39 +0900

    sort

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,5 +1,9 @@
 "use strict";
-console.log('hi there');
+//installed nodemon(allows rerun of node anytime file is changed), 
+//concurrently(multiple script runs at the same time) modules.
+//changed tsconfig.json rootDir, outDir
+console.log(123);
 var logSomething = function () {
     console.log('yoyo');
 };
+logSomething();
