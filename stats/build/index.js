fc231c48f3a5730bb639a626fb1e3ef0d51bf08b
jeongminhong <azurestefan@gmail.com>
2021-02-10 20:50:49 +0900

    readerutils

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,22 +1,35 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs")); //file system in node.
-var matches = fs_1.default.readFileSync('football.csv', {
-    encoding: 'utf-8'
-}).split('\n')
-    .map(function (row) {
-    return row.split(',');
-});
+var CsvFileReader_1 = require("./CsvFileReader"); //file system in node.
+var MatchResult_1 = require("./MatchResult");
+var reader = new CsvFileReader_1.CsvFileReader('football.csv');
+reader.read();
+//this is unclear code clarified code is below
+// for (let match of matches) {
+//     if(match[1] === 'Man United' && match[5] ==='H'){
+//         manUnitedWins++;
+//     } else if (match[2] === 'Man United' && match[5] === 'A'){
+//         manUnitedWins++;
+//     }
+// }
+//refactored code using typescript feature
+// const homeWin = 'H';
+// const awayWin = 'A';
+// const draw = 'D';
+// const MatchResult = {
+//     HomeWin: 'H',
+//     AwayWin: 'A',
+//     Draw: 'D'
+// };
+// identifiers/object above will be replaced with typescript's enum
+// enum is used over objects because it signals to engineers that these are a set of closely related values
 var manUnitedWins = 0;
-for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
-    var match = matches_1[_i];
-    if (match[1] === 'Man United' && match[5] === 'H') {
+for (var _i = 0, _a = reader.data; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === 'Man United' && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === 'Man United' && match[5] === 'A') {
+    else if (match[2] === 'Man United' && match[5] === MatchResult_1.MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,18 +1,37 @@
-import fs from 'fs'; //file system in node.
+import { CsvFileReader } from './CsvFileReader'; //file system in node.
+import { MatchResult } from './MatchResult';
 
-const matches = fs.readFileSync('football.csv', {
-    encoding: 'utf-8'
-}).split('\n')
-.map((row:string): string[] =>{
-    return row.split(',');
-})
+const reader = new CsvFileReader('football.csv');
+reader.read();
+
+//this is unclear code clarified code is below
+// for (let match of matches) {
+//     if(match[1] === 'Man United' && match[5] ==='H'){
+//         manUnitedWins++;
+//     } else if (match[2] === 'Man United' && match[5] === 'A'){
+//         manUnitedWins++;
+//     }
+// }
+
+//refactored code using typescript feature
+// const homeWin = 'H';
+// const awayWin = 'A';
+// const draw = 'D';
+// const MatchResult = {
+//     HomeWin: 'H',
+//     AwayWin: 'A',
+//     Draw: 'D'
+// };
+// identifiers/object above will be replaced with typescript's enum
+// enum is used over objects because it signals to engineers that these are a set of closely related values
+
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
-    if(match[1] === 'Man United' && match[5] ==='H'){
+for (let match of reader.data) {
+    if(match[1] === 'Man United' && match[5] ===MatchResult.HomeWin){
         manUnitedWins++;
-    } else if (match[2] === 'Man United' && match[5] === 'A'){
+    } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin){
         manUnitedWins++;
     }
 }
